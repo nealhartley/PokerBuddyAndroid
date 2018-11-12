657067d6d006c569b794f4d33274ef0a8e765a9d
nealhartley <neal.w.hartley@gmail.com>
2018-11-12 11:08:45 +1000

    Decided to take game logic out of card picker class. Added methods that control game flow and card picking in main activity.

--- MainActivity.java
+++ MainActivity.java
@@ -5,12 +5,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
     private PokerGame game;
+    private String stage = "DEAL_ONE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +26,61 @@ public class MainActivity extends AppCompatActivity {
 
         game = new PokerGame();
         sharedPreferences = this.getSharedPreferences("com.nealhartley.pokerbuddy", Context.MODE_PRIVATE);//THINKNEAL
+
+    }
+
+    //the button to select cards
+    public void start(View view){
+        Intent intent = new Intent(getApplicationContext(),CardPicker.class);
+        intent.putParcelableArrayListExtra("deck",game.getDeck());
+        startActivityForResult(intent,1);
+
     }
 
 
-    public void start(View view){
-        view.setVisibility(View.INVISIBLE);
-        Intent intent = new Intent(getApplicationContext(),CardPicker.class);
-        intent.putParcelableArrayListExtra("deck",game.getDeck());
-        startActivity(intent);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("resulting: ", "we should be putting ish down");
+        if (requestCode == 1) {
+            Log.i("resulting: ", "code was 1");
+            if(resultCode == RESULT_OK){
+                Log.i("resulting: ", "RESULT_OK");
+
+                Card cardReturned = new Card( data.getIntExtra("SelectedCardNumber",-1), data.getStringExtra("SelectedCardSuit") );
+                assignCard(cardReturned);
+
+            }
+        }
+    }
+
+    //assigns card text to correct place and updates game stage
+    private void assignCard(Card card) {
+
+        Log.i("assign card", "entered method");
+        Log.i("assign card", card.getValue() + " of " + card.getSuit());
+
+
+        switch (stage) {
+            case "DEAL_ONE":
+                TextView cardOne = (TextView) findViewById(R.id.cardOne);
+                cardOne.setText(card.getValue() + " of " + card.getSuit());
+
+                TextView title = (TextView) findViewById(R.id.handTitle);
+                title.setText("YOUR HAND");
+
+                stage = "DEAL_TWO";
+                break;
+            case "DEAL_TWO":
+                TextView cardTwo = (TextView) findViewById(R.id.cardTwo);
+                cardTwo.setText(card.getValue() + " of " + card.getSuit());
+
+                stage = "STAGE_THREE";
+                break;
+
+        }
+
+
 
     }
 
